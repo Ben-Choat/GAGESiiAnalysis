57e3e0e54d24c825fda6854b8850eed5c099a954
Ben Choat <ben.choat@gmail.com>
2023-10-24 22:26:14 -0600

    Updated code for IDing and plotting peaks

--- Python/Scripts/GAGESii_ExploreTree.py
+++ Python/Scripts/GAGESii_ExploreTree.py
@@ -14,7 +14,7 @@ from Load_Data import load_data_fun
 import glob
 import xgboost as xgb
 import matplotlib.pyplot as plt
-
+from scipy.signal import find_peaks
 
 
 
@@ -37,7 +37,7 @@ clust_meth =  'AggEcoregion' # 'Class' # 'None' # 'AggEcoregion', 'None',
 # CntlPlains, EastHghlnds, MxWdShld, NorthEast, SECstPlain, SEPlains, 
 # WestMnts, WestPlains, WestXeric 
 # define which region to work with
-region =  'SECstPlain' # 'WestMnts' # 'All' # 'CntlPlains' # 'Non-ref' # 'All'
+region =   'EastHghlnds' # 'SECstPlain' # 'WestMnts' # 'All' # 'CntlPlains' # 'Non-ref' # 'All'
              
 # define time scale working with. This vcombtrainariable will be used to read and
 # write data from and to the correct directories
@@ -73,17 +73,22 @@ model.load_model(
 # voi = 'WTDEPAVE'
 # voi = 'prcp'
 # voi = 'prcp_1'
-voi = 'TS_NLCD_21'
-# voi = 'TS_NLCD_24'
+# voi = 'RH_BASIN'
+# voi = 'TS_NLCD_21'
+voi = 'TS_NLCD_24'
 # voi = 'TS_NLCD_71'
 # voi = 'TS_NLCD_31'
 # voi = 'TS_NLCD_52'
 # voi = 'TS_NLCD_11'
+# voi = 'TS_NLCD_42' # evergreen forest
 # voi = 'TS_ag_irrig'
 # voi = 'TS_WaterUse_wu'
+# voi = 'TS_Population_PDEN'
 # voi = 'STOR_NID_2009'
 # voi = 'swe'
 # voi = 'SLOPE_PCT'
+# voi = 'vp'
+# voi = 'vp'
 # voi = 'BDAVE'
 # voi = 'TS_Housing_HDEN'
 # voi = 'CANALS_PCT'
@@ -116,99 +121,103 @@ df_tree = df_tree[df_tree['Feature'] != 'Leaf']
 # Calculate the count of values in the 'Category' column
 # value_counts = df_tree['Feature'].value_counts()[20:40]
 
-df_plot = df_treeGain.sort_values(
-    by = 'Gain', ascending = False
-    ).reset_index(drop = True)[120:130]
-df_plot.loc[0:125, 'Feature'].unique()
+# df_plot = df_treeGain.sort_values(
+#     by = 'Gain', ascending = False
+#     ).reset_index(drop = True)  # [120:130]
+# df_plot.loc[0:125, 'Feature'].unique()
 # Create a bar plot
-plt.bar(df_plot['Feat_Split'], df_plot['Gain'])
+# plt.bar(df_plot['Feat_Split'], df_plot['Gain'])
 
-# Adding labels and title
-plt.xlabel('Category')
-plt.ylabel('Gain')
-plt.title('Total gain at split value')
-# Rotate x-axis tick labels and anchor them to the end
-plt.xticks(rotation=45, horizontalalignment='right')
-# Show the plot
-plt.show()
+# # Adding labels and title
+# plt.xlabel('Category')
+# plt.ylabel('Gain')
+# plt.title('Total gain at split value')
+# # Rotate x-axis tick labels and anchor them to the end
+# plt.xticks(rotation=45, horizontalalignment='right')
+# # Show the plot
+# plt.show()
 
-#####################
+# #####################
 
-################################
-# gain of each split
-# Calculate the count of values in the 'Category' column
+# ################################
+# # gain of each split
+# # Calculate the count of values in the 'Category' column
+# # value_counts = df_tree['Feature'].value_counts()[20:40]
+
+# # Create a bar plot
+# plt.bar(
+#     df_tree.loc[df_tree['Feature'] == voi, 'Split'], 
+#     df_tree.loc[df_tree['Feature'] == voi, 'Gain'],
+#     width = 1)
+
+# # Adding labels and title
+# plt.xlabel('Split')
+# plt.ylabel('Gain')
+# plt.title(f'Gains at Specific Split Values - {voi} - {region}')
+# # Rotate x-axis tick labels and anchor them to the end
+# plt.xticks(rotation=45, horizontalalignment='right')
+# # Show the plot
+# plt.show()
+
+# #####################
+
+
+# ################################
+# # count of each var
+# # Calculate the count of values in the 'Category' column
 # value_counts = df_tree['Feature'].value_counts()[20:40]
 
-# Create a bar plot
-plt.bar(
-    df_tree.loc[df_tree['Feature'] == voi, 'Split'], 
-    df_tree.loc[df_tree['Feature'] == voi, 'Gain'],
-    width = 1)
+# # Create a bar plot
+# plt.bar(value_counts.index, value_counts.values)
 
-# Adding labels and title
-plt.xlabel('Split')
-plt.ylabel('Gain')
-plt.title(f'Gains at Specific Split Values - {voi} - {region}')
-# Rotate x-axis tick labels and anchor them to the end
-plt.xticks(rotation=45, horizontalalignment='right')
-# Show the plot
-plt.show()
+# # Adding labels and title
+# plt.xlabel('Category')
+# plt.ylabel('Count')
+# plt.title('Count of Splits Using Feature')
+# # Rotate x-axis tick labels and anchor them to the end
+# plt.xticks(rotation=45, horizontalalignment='right')
+# # Show the plot
+# plt.show()
 
-#####################
+# #####################
 
-
-################################
-# count of each var
-# Calculate the count of values in the 'Category' column
-value_counts = df_tree['Feature'].value_counts()[20:40]
-
-# Create a bar plot
-plt.bar(value_counts.index, value_counts.values)
-
-# Adding labels and title
-plt.xlabel('Category')
-plt.ylabel('Count')
-plt.title('Count of Splits Using Feature')
-# Rotate x-axis tick labels and anchor them to the end
-plt.xticks(rotation=45, horizontalalignment='right')
-# Show the plot
-plt.show()
-
-#####################
-
-print(df_tree.sort_values(
-    by = 'Gain', ascending = False
-    ).reset_index(drop = True)['Feature'].unique())
+# print(df_tree.sort_values(
+#     by = 'Gain', ascending = False
+#     ).reset_index(drop = True)['Feature'].unique())
 
 
-# subset trees to nodes where voi appears
+# # subset trees to nodes where voi appears
 df_tree_voi = df_tree[df_tree['Feature'] == voi]
 # df_tree_voi
 # df_tree_voi.sort_values(by = 'Gain', ascending = False)
 # df_tree_voi.sort_values(by = 'Tree')
 
-# print number of times each unique value of the voi was uesd for splitting
-# df_tree_voi.loc[df_tree_voi['Feature'] == 'DRAIN_SQKM', 'Split'].value_counts().head(20)
+# # print number of times each unique value of the voi was uesd for splitting
+# # df_tree_voi.loc[df_tree_voi['Feature'] == 'DRAIN_SQKM', 'Split'].value_counts().head(20)
 
 # sum total gain by each value of area used for splitting nodes
 df_splitGain = df_tree_voi.groupby('Split').sum('Gain').sort_values(
     by = 'Gain', ascending = False
     ).head(20)[['Gain']]
 df_splitGain = df_splitGain.reset_index()
-fig, ax = plt.subplots(figsize = [8,8])
-ax.plot(df_splitGain['Split'], df_splitGain['Gain'])
-ax.scatter(df_splitGain['Split'], df_splitGain['Gain'], color = 'orange')
-ax.set(xlabel = 'Split Values', ylabel = 'Gain at split', title = voi)
-plt.show()
+# fig, ax = plt.subplots(figsize = [8,8])
+# ax.plot(df_splitGain['Split'], df_splitGain['Gain'])
+# ax.scatter(df_splitGain['Split'], df_splitGain['Gain'], color = 'orange')
+# ax.set(xlabel = 'Split Values', ylabel = 'Gain at split', title = voi)
+# plt.show()
 
-import math
-# print the number of times each split occurs.
-nbins = math.ceil(math.log(df_tree_voi.shape[0], 2) + 1) * 2
-# df_tree_voi.value_counts('Split').hist(bins = nbins)
-ax = df_tree_voi.Split.hist(bins = nbins)
-ax.set_title(f'Counts of splits in {voi}')
-plt.show()
-df_tree_voi.value_counts('Split')
+# import math
+# # print the number of times each split occurs.
+# if df_tree_voi.shape[0] == 0:
+#     raise ValueError(
+#         'Splits based on \{voi} not found, try another feature.\n'
+#     )
+# nbins = math.ceil(math.log(df_tree_voi.shape[0], 2) + 1) * 2
+# # df_tree_voi.value_counts('Split').hist(bins = nbins)
+# ax = df_tree_voi.Split.hist(bins = nbins)
+# ax.set_title(f'Counts of splits in {voi}')
+# plt.show()
+# df_tree_voi.value_counts('Split')
 
 
 # %% find peaks and plot
@@ -217,42 +226,91 @@ df_tree_voi.value_counts('Split')
 # define data
 # Compute the histogram
 
-from scipy.signal import find_peaks
 
 df_plot = df_tree[
     df_tree['Feature'] == voi
+    # df_tree['Feature'].str.contains(voi)
     ].sort_values(by = 'Split').reset_index(drop = True)
 
 df_plot = df_plot.groupby(['Split'])['Gain'].sum().reset_index()
 
-x = df_plot['Split']
-y = df_plot['Gain']
+x_temp = df_plot['Split']
+y_temp = df_plot['Gain']
+
+
+
+# Sum y-values for ranges of 2 on the x-axis
+x = []
+y = []
+# if x_temp.max() > 
+range_len = np.round(x_temp.max()/100, 1) # length of values to sum over
+if range_len == 0: range_len = np.round(x_temp.max()/100, 3)
+
+# for i in range(0, int(np.ceil(x_temp.max())), range_len):
+for i in np.arange(0, np.ceil(x_temp.max() + range_len), range_len):
+    # low = y_temp[i]
+    # high = y_temp[i + range_len]
+    y.append(sum(y_temp[(x_temp >= i) & (x_temp < i+range_len)]))
+    # x.append(f'{i}-{i+range_len}')
+    x.append((i+(i+range_len))/2)
+print(x, y)
+
+# plt.bar(x, y)
+# plt.xticks(rotation=90)
+
 
 # calc distance threshold and prominence_threshold
-data_range = np.max(y) - np.min(y)
-distance_fraction = 0.0005  # 10% of data range
-prominence_fraction = 0.3  # 20% of data range
-distance_threshold = data_range * distance_fraction
-if distance_threshold < 20: distance_threshold = 20
-prominence_threshold = data_range * prominence_fraction
+y_range = np.max(y) - np.min(y)
+x_range = x_temp.max() - x_temp.min()
+distance_fraction = 0.0001  # e.g., 0.05% of data range
+prominence_fraction = 0.3  # e.g., 30% of data range
+height_in = 0.05 * y_temp.sum() # e.g., 15% total gain
+distance_threshold = x_range * distance_fraction
+if distance_threshold < 5: distance_threshold = 5
+prominence_threshold = y_range * prominence_fraction
 print(f'\n\ndistance_threshold: {distance_threshold}')
 print(f'\nprominence_threshold: {prominence_threshold}')
+print(f'\nheight: {height_in}')
 
-peaks, _ = find_peaks(
-    df_plot['Gain'], 
-    distance = distance_threshold, prominence = prominence_threshold
+
+# mirror the first 10 values to enable first value to be identified as peak
+# how many values to add to front?
+prpndN = 8
+
+y = [y[i] for i in range(prpndN-1, 0, -1)] + list(y)
+
+peaks, peakMetrics = find_peaks(
+    # df_plot['Gain'], 
+    y,
+    distance = distance_threshold, 
+    prominence = prominence_threshold,
+    height = height_in # 5000 # peak must have at least this much gain
     )
+peaks = peaks - (prpndN-1)
+
+y = y[prpndN-1:len(y)]
+
+peakSplits = np.array(x)[peaks]
 
 # plot
 plt.figure(figsize=(8, 4))
 
 # Plot the continuous data
 plt.plot(
+    # x_temp, y_temp,
     x, y,
     label='Continuous Data', color='b')
-plt.scatter(x.iloc[peaks], 
-            y.iloc[peaks], 
+# plt.scatter(x_temp, y_temp, 
+#             c='b', marker='o', label='Peaks')
+# plt.scatter(x_temp.iloc[peaks], 
+#             y_temp.iloc[peaks], 
+#             c='r', marker='o', label='Peaks')
+plt.scatter(x, y, 
+            c='b', marker='o')
+plt.scatter(np.array(x)[peaks], 
+            np.array(y)[peaks], 
             c='r', marker='o', label='Peaks')
+
 
 plt.title(f"Identifying Peaks in Split Values of {voi} Based on Gain")
 plt.xlabel("Split Value")
@@ -266,7 +324,7 @@ plt.show()
 # plot tree
 ##############
 
-fig, ax = plt.subplots(figsize = (100, 100))
-# xgb.plot_tree(model, num_trees = 89, ax = ax)
-xgb.plot_tree(model, num_trees = 749, ax = ax)
+# fig, ax = plt.subplots(figsize = (100, 100))
+# # xgb.plot_tree(model, num_trees = 89, ax = ax)
+# xgb.plot_tree(model, num_trees = 749, ax = ax)
 # %%
